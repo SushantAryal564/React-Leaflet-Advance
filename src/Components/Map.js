62af7a01fad38c144bd6b9ed65c304f01d5302d9
sushant <aryalsushant564@gmail.com>
2023-01-03 11:32:17 +0545

    Filtering functionality added

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,17 +1,23 @@
-import React from "react";
+import { React, useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import { cities } from "./../Data/cities";
 import MarkerLayer from "../Layers/marker_layer";
 import MarkerLayerWithTooltip from "../Layers/marker_layer_with_tooltips";
 import { mountains } from "../Data/highest_points";
 export const Map = (props) => {
+  const [radiusFilter, setRadiusFilter] = useState(null);
+  const getRadiusFilter = () => radiusFilter;
   return (
     <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <MarkerLayer features={cities.features} />
+      <MarkerLayer
+        features={cities.features}
+        setRadiusFilter={setRadiusFilter}
+        getRadiusFilter={getRadiusFilter}
+      />
       <MarkerLayerWithTooltip features={mountains.features} />
     </MapContainer>
   );
--- src/Layers/marker_layer.js
+++ src/Layers/marker_layer.js
@@ -1,27 +1,78 @@
 import { Marker, Popup } from "react-leaflet";
 import { defaultIcon } from "./../Components/icons/defaultIcon";
-import { Card } from "antd";
-const PopupStatistics = () => {
+import { Button, Card, InputNumber, Space } from "antd";
+import { Fragment, useState } from "react";
+import { FilterOutlined } from "@ant-design/icons";
+import L from "leaflet";
+const DEFAULTRadius = 3000;
+const PopupStatistics = ({ feature, setRadiusFilter }) => {
+  const [radius, setRadius] = useState(DEFAULTRadius);
+  const { ADM0NAME, NAME, POP_MAX } = feature.properties;
   return (
-    <Card type="inner" title="Name">
-      Inner Card content
-    </Card>
+    <Fragment>
+      <Card type="inner" title="Name" style={{ marginTop: 16 }}>
+        <b>{`${NAME}, ${ADM0NAME}`}</b>
+      </Card>
+      <Card type="inner" title="Population" style={{ marginTop: 16 }}>
+        <b>{`${POP_MAX}`}</b>
+      </Card>
+      <Card type="inner" title="Radius" style={{ marginTop: 16 }}>
+        <Space>
+          <InputNumber
+            defaultValue={DEFAULTRadius}
+            min={0}
+            onChange={(e) => setRadius(e)}
+          ></InputNumber>
+          <Button
+            type="primary"
+            shape="round"
+            icon={<FilterOutlined />}
+            onClick={() => setRadiusFilter({ feature, radius })}
+          >
+            Filter by km
+          </Button>
+        </Space>
+      </Card>
+    </Fragment>
   );
 };
-const MarkerLayer = ({ features }) => {
-  return features.map((feature) => {
-    const { coordinates } = feature.geometry;
-    return (
-      <Marker
-        key={String(coordinates)}
-        position={[coordinates[1], coordinates[0]]}
-        icon={defaultIcon}
-      >
-        <Popup>
-          <PopupStatistics />
-        </Popup>
-      </Marker>
-    );
-  });
+const MarkerLayer = ({ features, setRadiusFilter, getRadiusFilter }) => {
+  const radiusFilter = getRadiusFilter();
+  let centerPoint;
+
+  if (radiusFilter) {
+    const coordinates = radiusFilter.feature.geometry.coordinates;
+    centerPoint = L.latLng(coordinates[1], coordinates[0]);
+  }
+  return features
+    .filter((currentFeature) => {
+      if (centerPoint) {
+        const { coordinates } = currentFeature.geometry;
+        const currentPoint = L.latLng(coordinates[1], coordinates[0]);
+        console.log(centerPoint.distanceTo(currentPoint));
+        return (
+          centerPoint.distanceTo(currentPoint) / 1000 < radiusFilter.radius
+        );
+      } else {
+        return true;
+      }
+    })
+    .map((feature) => {
+      const { coordinates } = feature.geometry;
+      return (
+        <Marker
+          key={String(coordinates)}
+          position={[coordinates[1], coordinates[0]]}
+          icon={defaultIcon}
+        >
+          <Popup>
+            <PopupStatistics
+              feature={feature}
+              setRadiusFilter={setRadiusFilter}
+            />
+          </Popup>
+        </Marker>
+      );
+    });
 };
 export default MarkerLayer;
